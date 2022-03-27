526d86a79927008627b3fea618755b0f6db7e25a
LOMBAYE <lombayedev@gmail.com>
2022-03-27 13:42:45 +0000

    disabled a button switch cases

--- task.maker.js
+++ task.maker.js
@@ -13,11 +13,19 @@ const generators = document.querySelectorAll('.generator'),
 
 let nbr = 0;
 
-document.body.onload = () => {
-    generators[1].setAttribute('disabled', 'disabled')
-    generators[1].innerHTML = 'Add a column to start'
-}
+document.body.onload = () => disableAddTaskButton(nbr)
 
+function disableAddTaskButton(n) {
+    if (n === 0) {
+        generators[1].setAttribute('disabled', 'disabled')
+        generators[1].style.cursor = 'none'
+        generators[1].innerHTML = 'First add a column to start'
+    } else {
+        generators[1].innerHTML = 'Add a new task'
+        generators[1].removeAttribute('disabled')
+        generators[1].style.cursor = 'pointer'
+    }
+}
 generators[0].addEventListener('click', function() {
     nbr++
     addColumn();
@@ -29,31 +37,49 @@ generators[1].addEventListener('click', function() {
     })
 })
 
+function getColor(n) {
+    switch (n) {
+        case 1:
+            return ''
+        case 2:
+            return 'black'
+        case 3:
+            return 'red'
+        case 4:
+            return 'green'
+        case 5:
+            return 'blue'
+    }
+}
 
 function addColumn() {
-    generators[1].innerHTML = '+Add a task'
-    generators[1].removeAttribute('disabled')
+    disableAddTaskButton(nbr)
     if (nbr < 6) {
         const html = document.createElement('div'),
             title = document.createElement('input'),
             taches = document.createElement('div')
         html.classList.add('column')
+        html.setAttribute('id', 'column_' + nbr)
         title.classList.add('titre')
+        title.setAttribute('id', 'title_' + nbr)
         title.setAttribute('placeholder', 'Title of the list...')
-            // title.setAttribute('disabled', 'disabled')
         taches.classList.add('task')
-
+        taches.setAttribute('id', 'tache_' + nbr)
+        html.style.backgroundColor = getColor(nbr)
         main.appendChild(html)
         html.appendChild(title)
         html.appendChild(taches)
         console.log(nbr)
+        title.addEventListener('click', () => {
+            alert('remove Column')
+        })
     }
 }
 btn.addEventListener('click', (e) => {
     if (textarea.value === '' || textarea.value === 'Obligatoire') {
-        console.log(jour)
-            // console.log(debut.value)
-            // console.log(fin.value)
+
+        // console.log(debut.value)
+        // console.log(fin.value)
         e.preventDefault();
         textarea.style.color = 'red'
         textarea.textContent = 'Obligatoire'
@@ -62,15 +88,18 @@ btn.addEventListener('click', (e) => {
             textarea.style.color = 'black'
         })
     } else {
-
+        console.log(jour.value)
+        console.log(debut.value)
+        console.log(fin.value)
         console.log(textarea.value)
+        console.log(new Date())
         const note = document.createElement('div');
 
 
         console.log(note)
 
         note.classList.add('note')
-        task.appendChild(note)
+        main.appendChild(note)
         document.querySelector('.modal').style.display = 'none';
 
         // addTask();
